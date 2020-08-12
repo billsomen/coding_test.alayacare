fc0cea4d97aa6b34121f3b5d5cbfd562266a8a78
Bill <ngongangsomen@gmail.com>
2020-08-12 02:19:03 +0100

    first commit

---
+++ components/Logo.vue
@@ -0,0 +1,26 @@
+<template>
+  <!--  <vs-button circle icon success flat>-->
+  <vs-avatar circle>
+    <img :src="logoUri" alt="app-logo-uri" />
+  </vs-avatar>
+  <!--  </vs-button>-->
+  <!--<vs-button primary flat circle icon>
+    <vs-avatar circle>
+      <img :src="logoUri" alt="app-logo-uri" />
+    </vs-avatar>
+  </vs-button>-->
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'Logo',
+  data() {
+    return {
+      logoUri: '/alayacare.logo.jpg',
+    }
+  },
+})
+</script>
+
+<style scoped></style>
---
+++ components/_ui/HeaderButton.vue
@@ -0,0 +1,58 @@
+<template>
+  <vs-button danger flat class="header-button">
+    <div class="header-button-content">
+      <slot name="image"></slot>
+      <span v-if="text" class="text-view">
+        {{ text }}
+      </span>
+    </div>
+  </vs-button>
+  <!--<vs-button
+    class="header-button"
+    shadow
+    :active="active === 0"
+    @click="active = 0"
+  >
+    <div class="header-button-content">
+      <slot name="image"></slot>
+      <span v-if="text" class="text-view">
+        {{ text }}
+      </span>
+    </div>
+  </vs-button>-->
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'HeaderButton',
+  props: {
+    text: {
+      required: false,
+      type: String,
+      default: () => null,
+    },
+  },
+  data: () => ({
+    active: 0,
+  }),
+})
+</script>
+
+<style scoped>
+.header-button {
+  border-radius: 25px;
+  padding: 5px;
+  height: 50px;
+}
+
+.header-button-content {
+  display: flex;
+  align-items: center;
+  width: fit-content;
+  justify-content: space-between;
+}
+.text-view {
+  margin-left: 5px;
+}
+</style>
